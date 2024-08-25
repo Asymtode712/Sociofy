724b91681132c4625d544e1fba8621666756dd09
Siddheya Kulkarni <siddheyakul@gmail.com>
2024-08-25 15:06:01 +0530

    added deployed api

--- client/src/pages/loginPage/Form.jsx
+++ client/src/pages/loginPage/Form.jsx
@@ -57,7 +57,7 @@ const Form = () => {
          formData.append("picturePath", values.picture.name);
 
          const savedUserResponse = await fetch(
-          "http://localhost:3001/auth/register",
+          "https://sociofy-seven.vercel.app/auth/register",
           {
             method: "POST",
             body: formData,
@@ -73,7 +73,7 @@ const Form = () => {
 
     const login = async(values, onSubmitProps) => {
       const loggedInResponse = await fetch(
-       "http://localhost:3001/auth/login",
+       "https://sociofy-seven.vercel.app/auth/login",
        {
          method: "POST",
          headers: {"Content-Type": "application/json"},
